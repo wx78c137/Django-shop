a7a764ab6db598569f91f4a489a7b86b616452a2
MrVu <chuvi0902@gmail.com>
2019-01-12 12:30:30 +0700

    Add files via upload

--- shop/models.py
+++ shop/models.py
@@ -1,44 +1,37 @@
 from django.db import models
-from django.urls import reverse
+from shop.models import Product
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True ,db_index=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ('name', )
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
-
-
-class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, db_index=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=0)
-    available = models.BooleanField(default=True)
-    stock = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    image2 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+class Order(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.EmailField()
+    address = models.CharField(max_length=150)
+    postal_code = models.CharField(max_length=30)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ('name', )
-        index_together = (('id', 'slug'),)
+        ordering = ('-created', )
 
     def __str__(self):
-        return self.name
+        return 'Order {}'.format(self.id)
 
-    def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+
