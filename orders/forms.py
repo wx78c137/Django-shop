2d93c7e8ab2f3d7b6df8ad2f68c5a1c3256f5005
MrVu <chuvi0902@gmail.com>
2019-01-12 15:54:41 +0700

    change order form

--- orders/forms.py
+++ orders/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Order
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 
 
 class OrderCreateForm(forms.ModelForm):
