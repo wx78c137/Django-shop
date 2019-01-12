b8b3f65922b96ef5c7a387ae0d40b70afc437269
MrVu <chuvi0902@gmail.com>
2019-01-12 15:44:03 +0700

    change order form

--- orders/forms.py
+++ orders/forms.py
@@ -6,4 +6,9 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'phone_number', 'city']
+		
+		def __init__(self, *args, **kwargs):
+			super(ExampleForm, self).__init__(*args, **kwargs)
+			for visible in self.visible_fields():
+				visible.field.widget.attrs['class'] = 'form-control'
 
