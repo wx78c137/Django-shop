83bb5d70b656c3d9c8c2d5c2833de30689a7dbca
MrVu <chuvi0902@gmail.com>
2019-01-12 17:49:53 +0700

    gunicorn deploy

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
-
+import os,sys
+sys.path.append('/home/vivuvoveva1991/django-cart/lib/python3.5/site-packages')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
