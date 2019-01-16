0e4d2885f532e059fa02dc6c8f986448fff986cd
MrVu <chuvi0902@gmail.com>
2019-01-17 01:27:47 +0700

    fix nav menu

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,6 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True).order_by('-created_at')
-    print(products)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category)
