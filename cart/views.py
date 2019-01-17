adfcb59319f07249af6ec582dba5bc4fa11e3cd1
MrVu <chuvi0902@gmail.com>
2019-01-17 13:18:35 +0700

    debug

--- cart/views.py
+++ cart/views.py
@@ -25,7 +25,7 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
-    categories = Category.Objects.all()
+    categories = Category.objects.all()
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
         return render(request, 'cart/detail.html', {'cart': cart, 'categories': categories})
