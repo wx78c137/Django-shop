1fb430f9eb12b49f1f949e293142d252decaffee
MrVu <chuvi0902@gmail.com>
2019-01-17 16:54:50 +0700

    test footer

--- cart/views.py
+++ cart/views.py
@@ -26,11 +26,9 @@ def cart_remove(request, product_id):
 def cart_detail(request):
     cart = Cart(request)
     categories = Category.objects.all()
-    if cart is None:
-        return redirect('shop: product_list')
-    else:
-        for item in cart:
-            item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
-            return render(request, 'cart/detail.html', {'cart': cart, 'categories': categories})
+    
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
+    return render(request, 'cart/detail.html', {'cart': cart, 'categories': categories})
 
 
