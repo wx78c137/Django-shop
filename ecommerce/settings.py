096f55f492c5e3cb5dcd8e5cf6b70bbc04845727
vuhoang17891 <vuhoang17891@gmail.com>
2019-03-06 17:38:28 +0000

    settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jf$ac236s)uv)6xa&o0#@wmt+37zvlwf=atbi)%et^7=%3#$bb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.236.172.46']
+ALLOWED_HOSTS = ['beautybeauty.vn']
 
 
 # Application definition
